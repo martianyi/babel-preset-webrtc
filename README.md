61d7885d9af549b6a3a364999594e52d67edd5ed
Yi Kai <beliefs111@gmail.com>
2016-12-02 17:26:19 +0800

    add external helper

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # babel-preset-webrtc
 
-Babel preset for browsers support webrtc, namely Chrome 47+ and Firefox 49+
+Babel preset for browsers support webrtc, namely Chrome 47+ and Firefox 49+.
+Also this preset is intended to be using with rollup. 
+
+`npm install --save babel-preset-webrtc`
+
+
--- index.js
+++ index.js
@@ -1,9 +1,6 @@
 "use strict";
 
 const dependencies = require('./package.json')['dependencies'];
-const plugins = Object.keys(dependencies).map((plugin) => {
-    return require(plugin);
-});
 module.exports = {
-    plugins: plugins
+    plugins: Object.keys(dependencies).map(plugin=>require(plugin))
 };
